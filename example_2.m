dcc1ac9d525950895050df3dbfbcfdee26a42251
Gianluca Garofalo <gianluca.garofalo@dlr.de>
2019-10-10 18:34:26 +0200

    removed dependencies from lib functions

--- example_2.m
+++ example_2.m
@@ -1,9 +1,9 @@
-load('matlab.mat')
+load('example_2.mat')
 
-FPS = 20;
+FPS = 15;
 T = t(1) - 1/FPS - eps;
 X = -7:1e-3:7;
-dist = DistFcn( X(X>=0) );
+dist = X(X>=0).^2;
 
 fig = figure( 99 );
 clf( fig )
@@ -12,25 +12,28 @@ txt_col = 'w';
 line_col = [0 1 1];
 dot_col = [.8 .5 0];
 
-figx = MyAxes( [0.53 0.1 0.4 0.4], txt_col );
+figx = axes( 'Position', [0.53 0.1 0.4 0.4], 'Color', 'none' );
+xlabel( '$\xi$', 'Interpreter', 'latex' )
+ylabel( '$\dot \xi$', 'Interpreter', 'latex' )
+h1 = animatedline( figx, 'Color', line_col, 'LineWidth', 1.2 );
 axis( [0 x(1) -5 1] ), grid
 axis manual
-MyLabel( '$\xi_1$', '$\dot \xi_1$', txt_col )
-h1 = MyLine( figx, line_col );
 
-gauss = MyAxes( [0.53 0.65 0.4 0.3], txt_col );
+gauss = axes( 'Position', [0.53 0.65 0.4 0.3], 'Color', 'none' );
+xlabel( '$\xi$', 'Interpreter', 'latex' )
+ylabel( '$e^{-V(\xi,\sigma)}$', 'Interpreter', 'latex' )
+h2 = animatedline( gauss, 'Color', line_col, 'LineWidth', 1.2 );
+h3 = animatedline( gauss, 'MarkerFaceColor', dot_col, 'Color', dot_col, 'Marker', 'o' );
+an = text( x(1)+0.1, 0.07, '$\leftarrow \xi(t)$', 'Interpreter', 'latex', 'Rotation', 45 );
 axis( [0 x(1) 0 1] ), grid
 axis manual
-MyLabel( '$\xi_1$', '$e^{-V(\xi_1,\sigma)}$', txt_col )
-h2 = MyLine( gauss, line_col );
-h3 = animatedline( gauss, 'MarkerFaceColor', dot_col, 'Color', dot_col, 'Marker', 'o' );
-an = text( x(1)+0.1, 0.07, '$\leftarrow \xi_1(t)$', 'Interpreter', 'latex', 'Rotation', 45, 'Color', txt_col );
 
-state = MyAxes( [0.07 0.1 0.35 0.4], txt_col );
+state = axes( 'Position', [0.07 0.1 0.35 0.4], 'Color', 'none' );
+xlabel( '$\xi$', 'Interpreter', 'latex' )
+ylabel( '$\sigma$', 'Interpreter', 'latex' )
+h4 = animatedline( state, 'Color', line_col, 'LineWidth', 1.2 );
 axis( [-7.5 7.5 -5 5] ), grid
 axis manual
-MyLabel( '$\xi_1$', '$\sigma$', txt_col )
-h4 = MyLine( state, line_col );
 
 m = 7.5;
 Y = sqrt( 2*abs( log(k3/k2) )*( X.^2 + sb^2 ) );
@@ -39,17 +42,20 @@ fill( Y, X, 'r', 'FaceAlpha', 0.1 )
 fill( -Y, X, 'r', 'FaceAlpha', 0.1 )
 fill( [m Y m -m -Y -m], [-m X m m -X -m], 'b', 'FaceAlpha', 0.1 )
 
-eq = '$V(\xi_1,\sigma) = \frac{\xi_1^2}{2(\bar \sigma^2 + \sigma^2)}$';
-MyAnnotation( eq, [.07 .85 1 1], txt_col )
-eq = '$\dot \xi_1 = - k_1 e^{-V(\xi_1,\sigma)} \xi_1$';
-MyAnnotation( eq, [.07 .7 1 1], txt_col )
-eq = '$\dot \sigma = \big( k_3 - k_2 e^{-V(\xi_1,\sigma)} \big) \sigma$';
-MyAnnotation( eq, [.07 .6 1 1], txt_col )
+eq = '$V(\xi,\sigma) = \frac{\xi^2}{2(\bar \sigma^2 + \sigma^2)}$';
+annotation( 'textbox', [.07 .85 1 1], 'String', eq, 'LineStyle', 'none',...
+    'Interpreter', 'latex', 'VerticalAlignment', 'bottom', 'FontSize', 12 )
+eq = '$\dot \xi = - k_1 e^{-V(\xi,\sigma)} \xi$';
+annotation( 'textbox', [.07 .7 1 1], 'String', eq, 'LineStyle', 'none',...
+    'Interpreter', 'latex', 'VerticalAlignment', 'bottom', 'FontSize', 12 )
+eq = '$\dot \sigma = \big( k_3 - k_2 e^{-V(\xi,\sigma)} \big) \sigma$';
+annotation( 'textbox', [.07 .6 1 1], 'String', eq, 'LineStyle', 'none',...
+    'Interpreter', 'latex', 'VerticalAlignment', 'bottom', 'FontSize', 12 )
 
-opt = matlab2animate( 'make', 'background', 'skip', {'animatedline','text'}, 'bounding_box', [-1.4 -1.1 10.85 6.1] );
+opt = matlab2animate( 'make', 'background', 'skip', {'animatedline','text'} );
 opt.make = 'frame';
 
-for k = 1:5000:length(t)
+for k = 1:length(t)
     if t(k)-T>=1/FPS
         s2 = sb^2 + s(k)^2;
         y = exp( -dist/(2*s2) );
@@ -71,7 +77,21 @@ end
 hold( 'off' )
 close( fig );
 matlab2animate( 'make', 'root', 'fps', FPS );
-matlab2animate( 'make', 'adjust', 'old', {'font=\color{white}' 'draw=black'}, 'new', {'fill=none' 'draw=none'} );
+matlab2animate( 'make', 'adjust', 'old',...
+    {'font=\color{white!15!black}'  'draw=black'}, 'new',...
+    {'fill=none'                    'draw=none'} );
+matlab2animate( 'make', 'adjust', 'old',...
+    {'width=0.642\figH,' 'at={(0.738\figH,'  'width=0.543\figH,'}, 'new',...
+    {'width=0.62\figH,'  'at={(0.713\figH,'  'width=0.562\figH,'} );
+matlab2animate( 'make', 'adjust', 'old',...
+    {'at={(0.713\figH,' 'at={(-0.109\figH,' ',0\figH)},'}, 'new',...
+    {'at={(1.25\figH,'   'at={(-0.22\figH,' ',-0.2\figH)},'} );
+matlab2animate( 'make', 'adjust', 'old',...
+    {'\node[right, align=left, rotate=45]'                                              '{$\leftarrow \xi(t)$};'}, 'new',...
+    {['\begin{pgfonlayer}{foreground}' newline '\node[right, align=left, rotate=45]']   ['{$\leftarrow \xi(t)$};' newline '\end{pgfonlayer}']} );
+matlab2animate( 'make', 'adjust', 'old',...
+    '\begin{tikzpicture}', 'new',...
+    ['\begin{tikzpicture}' newline '\useasboundingbox (-1.0,-1.9) rectangle (8.8,4.5);'] );
 if isunix
     !make
 end
